da2187ad5024e0047d2eb58ee0b3de13313fa1f6
Mateus Moreira <mateusmoreirav@gmail.com>
2020-12-05 18:41:29 -0300

    Fix: Alterando para reutilizar o objeto do payload

--- src/routes/expenses/__tests__/CreateExpenseRouter.ts
+++ src/routes/expenses/__tests__/CreateExpenseRouter.ts
@@ -3,16 +3,17 @@ import app from '../../../server'
 
 describe('Create a Expense', () => {
   it('Should create a expense on endpoint', async () => {
-    const response = await request(app)
-      .post('/expenses')
-      .send({title: 'Padaria', description: 'Cafe na Padaria', tag: 'comida', value: 5.5})
-
-    expect(response).toEqual({
-      id: 1,
+    const expense = {
       title: 'Padaria',
       description: 'Cafe na Padaria',
       tag: 'comida',
       value: 5.5
-    })
+    }
+
+    const response = await request(app).post('/expenses').send(expense)
+
+    expect(response).toEqual(expense)
+
+    expect(response).toEqual(expect.any(Number))
   })
 })
--- src/routes/expenses/__tests__/CreateExpenseService.ts
+++ src/routes/expenses/__tests__/CreateExpenseService.ts
@@ -4,19 +4,17 @@ describe('Create a Expense', () => {
   it('Should create a expense', async () => {
     const createExpense = new CreateExpenseService()
 
-    const response = createExpense.execute({
+    const expense = {
       title: 'Padaria',
       description: 'Cafe na Padaria',
       tag: 'comida',
       value: 5.5
-    })
+    }
 
-    expect(response).toEqual({
-      id: 1,
-      title: 'Padaria',
-      description: 'Cafe na Padaria',
-      tag: 'comida',
-      value: 5.5
-    })
+    const response = createExpense.execute(expense)
+
+    expect(response).toEqual(expense)
+
+    expect(typeof response.id).toBe('number')
   })
 })
